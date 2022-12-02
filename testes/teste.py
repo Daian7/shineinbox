6553e089ec71f2abfb59b4884ca74300b27b55a3
Daian Mauro de Almeida <daian.mauro@outlook.com>
2022-12-01 21:52:30 -0300

    inclusão teste tela sobre

--- testes/teste.py
+++ testes/teste.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome('./chromedriver.exe')
 driver.get('http://127.0.0.1:5500/app/index.html')
+
+about_element = driver.find_element("id", "about")
+time.sleep(2)
+about_element.click()
+time.sleep(2)
+print('--------------- TELA SOBRE EXECUTADA ---------------')
